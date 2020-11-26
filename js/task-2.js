af336fef27e82dee527626e0d8ae33140a541c22
Корнеева Светлана <s.korneeva@brsm.com>
2020-11-26 10:44:08 +0200

    2d done

--- js/task-2.js
+++ js/task-2.js
@@ -1,27 +1,21 @@
 const ingredients = [
-  'Картошка',
-  'Грибы',
-  'Чеснок',
-  'Помидоры',
-  'Зелень',
-  'Приправы',
+  "Картошка",
+  "Грибы",
+  "Чеснок",
+  "Помидоры",
+  "Зелень",
+  "Приправы",
 ];
-console.log(ingredients);
+// console.log(ingredients);
 
-const createList = ingredient => {
-    const itemEl = document.createElement('li')
-    itemEl.textContent = el;
-    
-    return itemEl;
+const createList = (ingredient) => {
+  const itemEl = document.createElement("li");
+  itemEl.textContent = ingredient;
+
+  return itemEl;
 };
-const productList = ingredients.map(ingredient => createList(ingredient)); 
+const productList = ingredients.map((ingredient) => createList(ingredient));
 console.log(productList);
 
-const productListRef = document.querySelector('#ingredients');
+const productListRef = document.querySelector("#ingredients");
 productListRef.append(...productList);
-
-    
-    
-
-
-
